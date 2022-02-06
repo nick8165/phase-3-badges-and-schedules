dd9b07d1a3c2fbbc2a8227321f3da70ec8d55e4a
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-02-06 12:04:03 -0600

    c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+    array.map { |b| new_array.push(badge_maker(b)) }
+    new_array
+end
+
+def assign_rooms(speakers)
+    room_array = []
+    x = 1
+    while x <= speakers.length do
+        room_array.push("Hello, #{speakers[x - 1]}! You'll be assigned to room #{x}!")
+        x += 1
+    end
+    room_array
+end
+
+def printer(array)
+    new_badge = batch_badge_creator(array)
+    new_badge.map { |b| puts b}
+    new_room = assign_rooms(array)
+    new_room.map { |b| puts b}
+end
